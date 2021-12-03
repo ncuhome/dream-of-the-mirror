6a7215fc378849a828aaf8876f080b83149f4aaf
fxd <mygoddess007700@gmail.com>
2021-12-04 01:53:00 +0800

    导入2D sprite包

--- Assets/Scripts/DicButton.cs
+++ Assets/Scripts/DicButton.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class DicButton : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
