7df65d2748670cb217b02072a3cb32d595ccd89e
fxd <mygoddess007700@gmail.com>
2021-12-05 18:10:53 +0800

    feat: 新增通过固定轴获取位移改变量的脚本

---
+++ Assets/Scripts/MobileInputController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+[RequireComponent(typeof(UnityEngine.UI.AspectRatioFitter))]
+
+//轴控制脚本，利用轴图片和背景图片的位移差结合unity中的轴系统获取竖直和水平的位移变化量
+public class MobileInputController : MonoBehaviour,IBeginDragHandler,IDragHandler,IEndDragHandler,IPointerDownHandler,IPointerUpHandler 
+{
+    public RectTransform Background;
+    public RectTransform Knob;
+
+    [Header("Input Values")]
+    public float Horizontal = 0;
+    public float Vertical = 0;
+    public float offset;
+    Vector2 PointPosition;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+       
+        PointPosition = new Vector2((eventData.position.x - Background.position.x) / ((Background.rect.size.x - Knob.rect.size.x) / 2), (eventData.position.y - Background.position.y) / ((Background.rect.size.y - Knob.rect.size.y) / 2));
+
+        PointPosition = (PointPosition.magnitude>1.0f) ? PointPosition.normalized : PointPosition;
+
+        Knob.transform.position = new Vector2((PointPosition.x *((Background.rect.size.x-Knob.rect.size.x)/2)*offset) + Background.position.x, (PointPosition.y* ((Background.rect.size.y-Knob.rect.size.y)/2) *offset) + Background.position.y);
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        PointPosition = new Vector2(0f,0f);
+        Knob.transform.position = Background.position;
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        OnDrag(eventData);
+    }
+
+    public void OnPointerUp(PointerEventData eventData) 
+    {
+        OnEndDrag(eventData);
+    }
+   
+	void Update() 
+    {
+        Horizontal = PointPosition.x;
+        Vertical = PointPosition.y;
+    }
+
+    // public Vector2 Coordinate()
+    // {
+    //     return new Vector2(Horizontal, Vertical);
+    // }
+}
