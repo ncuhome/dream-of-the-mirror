6113d8ec6ce2fba29a97afd5d2fb0bbec3d79caf
fxd <mygoddess007700@gmail.com>
2021-12-06 15:41:59 +0800

    fix: 修改场景一的幕布默认大小（没什么用）

--- Assets/Scripts/GameScene1/MobileInputController.cs
+++ Assets/Scripts/GameScene1/MobileInputController.cs
@@ -3,6 +3,7 @@ using UnityEngine.EventSystems;
 [RequireComponent(typeof(UnityEngine.UI.AspectRatioFitter))]
 
 //轴控制脚本，利用轴图片和背景图片的位移差结合unity中的轴系统获取竖直和水平的位移变化量
+//使用前提是Canvas的尺寸为1，1，1
 public class MobileInputController : MonoBehaviour,IBeginDragHandler,IDragHandler,IEndDragHandler,IPointerDownHandler,IPointerUpHandler 
 {
     public RectTransform Background;
@@ -13,6 +14,7 @@ public class MobileInputController : MonoBehaviour,IBeginDragHandler,IDragHandle
     public float Vertical = 0;
     //控制轴可以移动的半径
     public float offset;
+
     Vector2 PointPosition;
 
     public void OnBeginDrag(PointerEventData eventData)
@@ -21,13 +23,19 @@ public class MobileInputController : MonoBehaviour,IBeginDragHandler,IDragHandle
     }
 
     public void OnDrag(PointerEventData eventData)
-    {
-       
+    {   
+        // print(eventData.position);
+        // print(Background.position);
+        
         PointPosition = new Vector2((eventData.position.x - Background.position.x) / ((Background.rect.size.x - Knob.rect.size.x) / 2), (eventData.position.y - Background.position.y) / ((Background.rect.size.y - Knob.rect.size.y) / 2));
 
         PointPosition = (PointPosition.magnitude>1.0f) ? PointPosition.normalized : PointPosition;
 
         Knob.transform.position = new Vector2((PointPosition.x *((Background.rect.size.x-Knob.rect.size.x)/2)*offset) + Background.position.x, (PointPosition.y* ((Background.rect.size.y-Knob.rect.size.y)/2) *offset) + Background.position.y);
+        // float t = (PointPosition.x *((Background.rect.size.x-Knob.rect.size.x)/2)*offset) + Background.position.x;
+        // float t = Background.rect.size.x-Knob.rect.size.x;
+        // print(t);
+        // print(Knob.transform.position);
     }
 
     public void OnEndDrag(PointerEventData eventData)
