c289fd4875f33a850cc9d783869bd9c85ced6a67
fxd <mygoddess007700@gmail.com>
2021-12-28 19:45:33 +0800

    :art: build: 从GirlHero剥离出翻滚脚本和剑攻击脚本，其它动作涉及多动画未剥离

---
+++ Assets/Scripts/ActionGame/Hero_Animator/GirlHero_Roll.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GirlHero_Roll : StateMachineBehaviour
+{
+    public GirlHero girlHero;
+    public Health health;
+    public float rollSpeed;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        girlHero = GameObject.FindGameObjectWithTag("Hero").GetComponent<GirlHero>();
+        
+        health = girlHero.playHealth;
+        health.invincible = true;
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Vector2 newPos = Vector2.MoveTowards(girlHero.rb.position, girlHero.rb.position + (Vector2)girlHero.transform.right, rollSpeed * Time.fixedDeltaTime);
+        girlHero.rb.MovePosition(newPos);
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        health.invincible = false;
+    }
+}
