334cf93c4ab040deaf49905fb6d8444a5c2b42ff
fxd <mygoddess007700@gmail.com>
2021-12-28 19:49:21 +0800

    :fire: Remove: 剥离一部分动画内发生的语句使之附着在动画上

--- Assets/Scripts/ActionGame/GirlHero.cs
+++ Assets/Scripts/ActionGame/GirlHero.cs
@@ -4,291 +4,265 @@ using UnityEngine;
 
 public class GirlHero : MonoBehaviour
 {
-  // For GroundSensor
-  public Rigidbody2D rb;
-  // 当前跳跃次数
-  public int currentJumpCount = 0;
-  // 是否落地（通过地面触发器）
-  public bool grounded = false;
-  // 水平运动比例
-  public float moveX;
-  //现在面朝方向（用于未移动时判定）
-  public Facing facing = Facing.Right;
+    // For GroundSensor
+    public Rigidbody2D rb;
+    public int currentJumpCount = 0;
+    public bool grounded = false;
 
-  // 通过翻滚协程判断翻滚是否结束
-  public bool rolled = false;
+    public CapsuleCollider2D capsuleCollider;
+    public Animator anim;
+    public Health playHealth;
 
-  public CapsuleCollider2D capsuleCollider;
-  public Animator anim;
-  public Health playHealth;
+    [Header("实例化的ButtonClickController脚本")]
+    public ButtonClickController jumpBtn;
+    public ButtonClickController rollBtn;
+    public ButtonClickController swordAttackBtn;
+    public ButtonClickController magicAttackBtn;
 
-  [Header("实例化的ButtonClickController脚本")]
+    [Header("[Setting]")]
+    // 左右移动速度
+    public float moveSpeed;
+    // 最大跳跃次数
+    public int maxJumpCount;
+    // 每秒攻击次数
+    public float attackRate;
+    // 跳跃升力
+    public float jumpForce;
 
-  public ButtonClickController jumpBtn;
-  public ButtonClickController rollBtn;
-  public ButtonClickController swordAttackBtn;
-  public ButtonClickController magicAttackBtn;
+    public float rollCd;
+    public float jumpCd;
 
-  [Header("线性变量")]
-  public float rollSpeed;
+    [Header("贴图默认朝向")]
+    public Facing facing;
 
-  [Header("[Setting]")]
-  // 左右移动速度
-  public float moveSpeed;
-  // 翻滚速度
-  public float rollForce;
-  // 最大跳跃次数
-  public int maxJumpCount;
-  // 跳跃升力
-  public float jumpForce;
-  // 每秒攻击次数
-  //修改同时也需要修改动画采样点个数
-  public float attackRate;
+    [Header("粒子")]
+    public GameObject dustEffect;
 
-  public float rollCd;
+    [Header("魔法冲击波")]
+    public GameObject boPrefab;
 
-  public float jumpCd;
+    [Header("音频")]
+    public AudioSource attackAudio;
+    public AudioSource jumpAudio;
+    public AudioSource runAudio;
+    public AudioSource rollAudio;
 
-  [Header("粒子")]
-  public GameObject dustEffect;
+    // 水平运动比例
+    private float moveX;
+    private float nextRollTime = 0f;
+    private float nextAttackTime = 0f;
 
-  [Header("需要填充: ")]
-  public GameObject boPrefab;
+    private bool spawnLandDust = false;
+    private ParticleSystem dust;
 
-  [Header("音频")]
-  public AudioSource attackAudio;
-  public AudioSource jumpAudio;
-  public AudioSource runAudio;
-  public AudioSource rollAudio;
+    private MobileHorizontalInputController inputController;
 
-  float nextRollTime = 0f;
-
-  float nextAttackTime = 0f;
-
-  Vector3 velocity = Vector3.zero;
-
-  bool spawnLandDust = false;
-
-  private ParticleSystem dust;
-
-
-  MobileHorizontalInputController inputController;
-
-  //暂时提供一下。。。
-  private bool curAnimIs(string animName)
-  {
-    return anim.GetCurrentAnimatorStateInfo(0).IsName(animName);
-  }
-
-  // 获取组件
-  void Start()
-  {
-    capsuleCollider = GetComponent<CapsuleCollider2D>();
-    anim = this.transform.Find("HeroModel").GetComponent<Animator>();
-    rb = GetComponent<Rigidbody2D>();
-
-    GameObject directionJoyStick = GameObject.Find("DirectionJoyStick");
-    inputController = directionJoyStick.GetComponent<MobileHorizontalInputController>();
-
-    var dustPos = new Vector3(transform.position.x, transform.position.y - 0.5f, transform.position.z);
-    var dustPrefab = Instantiate(dustEffect, dustPos, Quaternion.identity);
-    dustPrefab.transform.parent = transform;
-    dust = dustPrefab.GetComponent<ParticleSystem>();
-
-    playHealth = GetComponent<Health>();
-  }
-
-  void FixedUpdate()
-  {
-    if (PauseControl.gameIsPaused) return;
-
-    // 虚拟轴水平移动
-    if (inputController.dragging)
+    private bool curAnimIs(string animName)
     {
-      moveX = inputController.horizontal;
-    }
-    else
-    {
-      moveX = Input.GetAxisRaw("Horizontal"); // 键盘也算
+        return anim.GetCurrentAnimatorStateInfo(0).IsName(animName);
     }
 
-    // 左右水平移动（因为想要修改成攻击和翻滚不能移动）
-    if (moveX == 0
-        || curAnimIs("GirlHero_Sword")
-        || curAnimIs("GirlHero_Magic")
-        || curAnimIs("GirlHero_Roll"))
+    // 获取组件
+    void Start()
     {
-      anim.SetBool("Running", false);
-      StopAudio(runAudio);
-    }
-    else
-    {
-      if (!grounded)
-      {
-        anim.SetBool("Running", false);
-        StopAudio(runAudio);
-      }
-      else
-      {
-        anim.SetBool("Running", true);
+        rb = GetComponent<Rigidbody2D>();
+        playHealth = GetComponent<Health>();
+        capsuleCollider = GetComponent<CapsuleCollider2D>();
+        anim = transform.Find("HeroModel").GetComponent<Animator>();
+        
+        GameObject directionJoyStick = GameObject.FindGameObjectWithTag("DirectionJoyStick");
+        inputController = directionJoyStick.GetComponent<MobileHorizontalInputController>();
 
-        PlayAudio(runAudio);
-      }
-
-
-      facing = moveX > 0 ? Facing.Right : Facing.Left;
-
-      Flip(moveX > 0);
-
-
-      //先试试放到fixUpdate里面线性移动行不行
-      Vector2 tPos = transform.position;
-      tPos.x += moveX * moveSpeed * Time.fixedDeltaTime;
-      transform.position = tPos;
-
-      //暂时取消，后期迭代
-      // Vector3 targetVelocity = new Vector2(moveX * moveSpeed, rb.velocity.y);
-      // rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, .05f);
+        var dustPos = new Vector3(transform.position.x, transform.position.y - 0.5f, transform.position.z);
+        var dustPrefab = Instantiate(dustEffect, dustPos, Quaternion.identity);
+        dustPrefab.transform.SetParent(transform);
+        dust = dustPrefab.GetComponent<ParticleSystem>();
     }
 
-  }
-
-  void Update()
-  {
-    if (PauseControl.gameIsPaused) return;
-    if (Time.time >= nextAttackTime)
+    void FixedUpdate()
     {
-      if (Input.GetButton("Fire1") || swordAttackBtn.pressed)
-      {
-        SwordAttack();
-        nextAttackTime = GetNextTime(1f / attackRate);
-      }
-      if (Input.GetButton("Fire2") || magicAttackBtn.pressed)
-      {
-        MagicAttack();
-        nextAttackTime = GetNextTime(1f / attackRate);
-      }
+        if (PauseControl.gameIsPaused) return;
+
+        // 虚拟轴水平移动
+        if (inputController.dragging)
+        {
+            moveX = inputController.horizontal;
+        }
+        else
+        {
+            moveX = Input.GetAxisRaw("Horizontal");
+        }
+
+        // 左右水平移动（因为想要实现只有攻击和翻滚不能移动，其它情况下可以移动，且空中移动播放空中动画）
+        if (moveX == 0
+            || curAnimIs("GirlHero_Sword")
+            || curAnimIs("GirlHero_Magic")
+            || curAnimIs("GirlHero_Roll"))
+        {
+            anim.SetBool("Running", false);
+            StopAudio(runAudio);
+        }
+        else
+        {
+            if (!grounded)
+            {
+                anim.SetBool("Running", false);
+                StopAudio(runAudio);
+            }
+            else
+            {
+                anim.SetBool("Running", true);
+                PlayAudio(runAudio);
+            }
+
+            Flip(moveX > 0);
+
+            Vector2 newPos = Vector2.MoveTowards(rb.position, rb.position + (Vector2)transform.right, moveSpeed * Time.fixedDeltaTime);
+            rb.MovePosition(newPos);
+        }
     }
 
-    if (Time.time >= nextRollTime)
+    void Update()
     {
-      if (Input.GetButtonDown("Roll") || rollBtn.pressed)
-      {
-        //实现点一次按一下（可能不好。。。）
-        rollBtn.pressed = false;
-        Roll();
-        nextRollTime = GetNextTime(rollCd);
-      }
+        if (PauseControl.gameIsPaused) return;
+
+        //使人物可以识别持续碰撞
+        rb.WakeUp();
+        
+        if (Time.time >= nextAttackTime)
+        {
+            if (Input.GetButton("Fire1") || swordAttackBtn.pressed)
+            {
+                SwordAttack();
+                nextAttackTime = GetNextTime(1f / attackRate);
+            }
+            if (Input.GetButton("Fire2") || magicAttackBtn.pressed)
+            {
+                MagicAttack();
+                nextAttackTime = GetNextTime(1f / attackRate);
+            }
+        }
+
+        if (Time.time >= nextRollTime)
+        {
+            if (Input.GetButtonDown("Roll") || rollBtn.pressed)
+            {
+                //实现点一次按一下（可能不好。。。）
+                rollBtn.pressed = false;
+                Roll();
+                nextRollTime = GetNextTime(rollCd);
+            }
+        }
+
+        if (jumpBtn.pressed || Input.GetButtonDown("Jump"))
+        {
+            if (currentJumpCount < maxJumpCount)
+            {
+                jumpBtn.pressed = false;
+                // 跳跃行为
+                Jump();
+            }
+        }
+
+        if (grounded)
+        {
+            if (spawnLandDust)
+            {
+                spawnLandDust = false;
+                CreateDust();
+                PlayAudio(jumpAudio);
+            }
+        }
+        else
+        {
+            spawnLandDust = true;
+        }
     }
 
-    if (jumpBtn.pressed || Input.GetButtonDown("Jump"))
+    // TODO: finish SwordAttack
+    void SwordAttack()
     {
-      if (currentJumpCount < maxJumpCount)
-      {
-        //实现点一次按一下（可能不好。。。）
-        jumpBtn.pressed = false;
-        // 跳跃行为
-        Jump();
-      }
+        PlayAudio(attackAudio);
+
+        anim.SetTrigger("SwordAttack");
     }
 
-    if (grounded)
+    // TODO: finish MagicAttack
+    void MagicAttack()
     {
-      if (spawnLandDust)
-      {
-        spawnLandDust = false;
+        StartCoroutine(SpawnBo());
+        anim.SetTrigger("MagicAttack");
+    }
+
+    // TODO: Fix roll distance
+    void Roll()
+    {
+        PlayAudio(rollAudio);
+
+        anim.SetTrigger("Roll");
         CreateDust();
+    }
+
+    // TODO: Fix doubleJump
+    void Jump()
+    {
         PlayAudio(jumpAudio);
-      }
-    }
-    else
-    {
-      spawnLandDust = true;
-    }
-  }
 
-  // TODO: finish SwordAttack
-  void SwordAttack()
-  {
-    anim.SetTrigger("SwordAttack");
-    PlayAudio(attackAudio);
-  }
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        currentJumpCount++;
 
-  // TODO: finish MagicAttack
-  void MagicAttack()
-  {
-    StartCoroutine(SpawnBo());
-    anim.SetTrigger("MagicAttack");
-  }
-
-  // TODO: Fix roll distance
-  void Roll()
-  {
-    PlayAudio(rollAudio);
-
-    anim.SetTrigger("Roll");
-    CreateDust();
-
-    // StartCoroutine(RollMove());
-    //暂时注销，因为摄像机会有抖动，之后处理抖动后再加上，现在改为使用movement协程（不知道有没有问题。。。）
-    rb.AddForce(new Vector2((int)facing * rollForce, 0), ForceMode2D.Impulse);
-  }
-
-  // TODO: Fix doubleJump
-  void Jump()
-  {
-    PlayAudio(jumpAudio);
-
-    anim.SetTrigger("Jump");
-    CreateDust();
-
-    rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-    currentJumpCount++;
-  }
-
-  // Flip 参数表示此时在哪边，会把人物翻转到另一边
-  void Flip(bool right)
-  {
-    float next = right ? 0 : 180;
-    if (transform.rotation.eulerAngles.y != next)
-    {
-      if (grounded)
-      {
+        anim.SetTrigger("Jump");
         CreateDust();
-      }
-      transform.rotation = Quaternion.Euler(0, next, 0);
     }
-  }
 
-  void CreateDust()
-  {
-    dust.Play();
-  }
-
-  float GetNextTime(float offset)
-  {
-    return Time.time + offset;
-  }
-
-  IEnumerator SpawnBo()
-  {
-    yield return new WaitForSeconds(1.0f / attackRate - 0.1f);
-    Instantiate(boPrefab, transform.position + transform.right, transform.rotation);
-  }
-
-  void PlayAudio(AudioSource t)
-  {
-    if (t != null && !t.isPlaying)
+    // Flip 参数表示此时在哪边，会把人物翻转到另一边
+    void Flip(bool right)
     {
-      t.Play();
+        if (facing == Facing.Left)
+        {
+            right = !right;
+        }
+        
+        float next = right ? 0 : 180;
+        if (transform.rotation.eulerAngles.y != next)
+        {
+            if (grounded)
+            {
+                CreateDust();
+            }
+            transform.rotation = Quaternion.Euler(0, next, 0);
+        }
     }
-  }
 
-  void StopAudio(AudioSource t)
-  {
-    if (t != null && t.isPlaying)
+    void CreateDust()
     {
-      t.Stop();
+        dust.Play();
     }
-  }
+
+    float GetNextTime(float offset)
+    {
+        return Time.time + offset;
+    }
+
+    IEnumerator SpawnBo()
+    {
+        yield return new WaitForSeconds(1.0f / attackRate - 0.1f);
+        Instantiate(boPrefab, transform.position + transform.right, transform.rotation);
+    }
+
+    void PlayAudio(AudioSource t)
+    {
+        if (t != null && !t.isPlaying)
+        {
+            t.Play();
+        }
+    }
+
+    void StopAudio(AudioSource t)
+    {
+        if (t != null && t.isPlaying)
+        {
+            t.Stop();
+        }
+    }
 }
