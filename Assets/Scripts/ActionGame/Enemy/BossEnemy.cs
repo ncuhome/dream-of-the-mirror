a56a4560cc0dc61c8db9639e7efc23f96c3e1b56
fxd <mygoddess007700@gmail.com>
2021-12-29 13:41:50 +0800

    :fire: refactor: 简化判定语句
    c

---
+++ Assets/Scripts/ActionGame/Enemy/BossEnemy.cs
@@ -0,0 +1,95 @@
+using UnityEngine;
+
+public class BossEnemy : Enemy
+{
+    [Header("Boss: ")]
+    //Boss最少攻击思考时间
+    public float timeThinkMin = 4f;
+    //Boss最大攻击思考时间
+    public float timeThinkMax = 6f;
+    public float attackRange = 1.5f;
+    public float attackCd = 0.5f;
+    public int attackDamage = 1;
+    public Health playerHealth;
+
+    public float timeNextDecision = 0;
+
+    private float nextAttackTime;
+    //远程两种方式
+    private string[] remoteAttack = new string[]{
+        "Magic", "Teleport"
+    };
+
+    protected override void Start()
+    {
+        base.Start();
+        playerHealth = GetComponent<Health>();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+        if (enemyAttackConsciousness.heroDistance > enemyAttackConsciousness.attackConsciousnessRange)
+        {
+            return;
+        }
+        if (playerHealth.currentHealth <= playerHealth.maxHealth/2)
+        {
+            timeThinkMin /= 2;
+            timeThinkMax /= 2;
+        }
+        if (!curAnimIs("Boss_Teleport"))
+        {
+            //判断贴图方向
+            Flip((girlHero.transform.position.x - transform.position.x) > 0);
+        }
+
+        //特殊动作判定
+        if (Time.time > timeNextDecision)
+        {
+            anim.SetTrigger(remoteAttack[Random.Range(0, remoteAttack.Length)]);
+
+            //进行这一次攻击，同时为下一次攻击做准备
+            float thinkTime = Random.Range(timeThinkMin, timeThinkMax);
+            timeNextDecision = Time.time + thinkTime;
+        }
+
+        //攻击判定
+        if (!curAnimIs("Boss_Magic") && Time.time >= nextAttackTime)
+        {
+            if (enemyAttackConsciousness.heroDistance < attackRange)
+            {
+                anim.SetTrigger("Attack");
+                nextAttackTime += attackCd;
+            }
+        }
+    }
+
+    protected override void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (!curAnimIs("Boss_Attack"))
+        {
+            return;
+        }
+        base.OnTriggerEnter2D(other);
+    }
+
+    public void OnTriggerStay2D(Collider2D other)
+    {
+        if (!curAnimIs("Boss_Attack"))
+        {
+            return;
+        }
+        if (other.gameObject.GetComponent<Health>() == null)
+        {
+            return;
+        }
+        if (other.tag == "Hero")
+        {
+            if (Time.time > other.gameObject.GetComponent<Health>().nextInvincibleTime)
+            {
+                other.GetComponent<Health>().TakeDamage(attackDamage);
+            }
+        }   
+    }
+}
