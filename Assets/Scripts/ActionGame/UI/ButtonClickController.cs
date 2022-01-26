5193102ac3fb85a592629a27a97b3d709d3b52ef
fxd <mygoddess007700@gmail.com>
2022-01-26 13:57:48 +0800

    :sparkles: feat: 增加按钮长按功能

--- Assets/Scripts/ActionGame/UI/ButtonClickController.cs
+++ Assets/Scripts/ActionGame/UI/ButtonClickController.cs
@@ -5,13 +5,28 @@ public class ButtonClickController : MonoBehaviour, IPointerDownHandler, IPointe
 {
     public bool pressed; //等价与Input.GetKey()
 
+    public float thinkTime = 0.05f; //判断长按还是短按的时间
+    public bool isStart = false;
+    public float lastTime = 0;
+
     public void OnPointerDown(PointerEventData eventData)
     {
+        LongPress(true);
         pressed = true;
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
         pressed = false;
+        if (isStart)
+        {
+            LongPress(false);
+        }
+    }
+
+    public void LongPress(bool bStart)
+    {
+        isStart = bStart;
+        lastTime = Time.time;
     }
 }
