4c443f40af69110b86e764c32618b35d7b30b580
fxd <mygoddess007700@gmail.com>
2022-01-26 14:13:20 +0800

    :bug: fix: 修复落地问题，该脚本目前只控制玩家是否落地，后期可能需要转换这个脚本

--- Assets/Scripts/ActionGame/Player/GroundSensor.cs
+++ Assets/Scripts/ActionGame/Player/GroundSensor.cs
@@ -10,6 +10,18 @@ public class GroundSensor : MonoBehaviour
         root = this.transform.root.GetComponent<GirlHero>();
     }
 
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Ground"))
+        {
+            if (root.rb.velocity.y < 0)
+            {
+                root.grounded = true;
+                root.anim.SetBool("Grounded", true);
+            }
+        }
+    }
+
     void OnTriggerStay2D(Collider2D other)
     {
         if (other.CompareTag("Ground"))
@@ -27,5 +39,7 @@ public class GroundSensor : MonoBehaviour
     {
         root.grounded = false;
         root.anim.SetBool("Grounded", false);
+        //因为第一次增加会被stay抵消，后期需要修改
+        root.currentJumpCount = 1;
     }
 }
