03ac9a26db1e9f2e62d898a023a98a4592bfb8a2
fxd <mygoddess007700@gmail.com>
2022-01-26 14:01:19 +0800

    :memo: Doc: 添加注释

--- Assets/Scripts/ActionGame/UI/ButtonClickController.cs
+++ Assets/Scripts/ActionGame/UI/ButtonClickController.cs
@@ -3,6 +3,21 @@ using UnityEngine.EventSystems;
 
 public class ButtonClickController : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
+    //长按与短按的判定：
+    // if (!isJump)
+    // {
+    //     if (!jumpBtn.isStart)
+    //     {
+    //         rb.AddForce(Vector2.up * shortJumpForce, ForceMode2D.Impulse);
+    //         isJump = true;
+    //     }
+    //     else if (Time.time - jumpBtn.lastTime > jumpBtn.thinkTime)
+    //     {
+    //         jumpBtn.isStart = false;
+    //         rb.AddForce(Vector2.up * longJumpForce, ForceMode2D.Impulse);
+    //         isJump = true;
+    //     }
+    // }
     public bool pressed; //等价与Input.GetKey()
 
     public float thinkTime = 0.05f; //判断长按还是短按的时间
