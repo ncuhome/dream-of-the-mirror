5c1e2cbe3a918a3ecc402a7aba987a7b1a8d898e
fxd <mygoddess007700@gmail.com>
2022-01-26 13:56:36 +0800

    :sparkles: feat: 增加摄像机随人物移动的水平偏移

--- Assets/Scripts/ActionGame/CameraController.cs
+++ Assets/Scripts/ActionGame/CameraController.cs
@@ -6,13 +6,42 @@ public class CameraController : MonoBehaviour
     public GameObject target;
 
     public float smoothTime = 0.05f;
-    public float posY = 1;
+    public float posY = 1f;
+    
+    public float horizontalOffset = 1f;
 
-    private Vector3 cameraVelocity = Vector3.zero;
+    private MobileHorizontalInputController inputController;
+    private float horizontalOffsetDir = 1f;
+    private float tXDir = 0;
+
+    void Start()
+    {
+        GameObject directionJoyStick = GameObject.FindGameObjectWithTag("DirectionJoyStick");
+        inputController = directionJoyStick.GetComponent<MobileHorizontalInputController>();    
+    }
 
     void FixedUpdate()
     {
-        Vector3 targetPos = new Vector3(target.transform.position.x, target.transform.position.y + posY, -100);
+        // 虚拟轴水平移动
+        if (inputController.dragging)
+        {
+            tXDir = inputController.horizontal;
+        }
+        else
+        {
+            tXDir = Input.GetAxisRaw("Horizontal");
+        }
+
+        if (tXDir > 0)
+        {
+            horizontalOffsetDir = 1;
+        }
+        if (tXDir < 0)
+        {
+            horizontalOffsetDir = -1;
+        }
+
+        Vector3 targetPos = new Vector3(target.transform.position.x + horizontalOffset * horizontalOffsetDir, target.transform.position.y + posY, -100);
         transform.position = Vector3.Lerp(transform.position, targetPos, Time.deltaTime * smoothTime * 100);
         rectTransform.position = (Vector2)transform.position;
     }
