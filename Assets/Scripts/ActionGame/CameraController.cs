f65572e76b292a24f4e413b7419ac74ed4bcc641
fxd <mygoddess007700@gmail.com>
2022-02-24 19:36:53 +0800

    :fire: refactor: 简化摄像机移动代码

--- Assets/Scripts/ActionGame/CameraController.cs
+++ Assets/Scripts/ActionGame/CameraController.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public RectTransform rectTransform;
     public GameObject target;
 
     public float smoothTime = 0.05f;
@@ -12,7 +11,6 @@ public class CameraController : MonoBehaviour
 
     private MobileHorizontalInputController inputController;
     private float horizontalOffsetDir = 1f;
-    private float tXDir = 0;
 
     void Start()
     {
@@ -20,29 +18,20 @@ public class CameraController : MonoBehaviour
         inputController = directionJoyStick.GetComponent<MobileHorizontalInputController>();    
     }
 
-    void FixedUpdate()
+    void Update()
     {
         // 虚拟轴水平移动
         if (inputController.dragging)
         {
-            tXDir = inputController.horizontal;
+            horizontalOffsetDir = inputController.horizontal;
         }
         else
         {
-            tXDir = Input.GetAxisRaw("Horizontal");
+            horizontalOffsetDir = Input.GetAxisRaw("Horizontal");
         }
 
-        if (tXDir > 0)
-        {
-            horizontalOffsetDir = 1;
-        }
-        if (tXDir < 0)
-        {
-            horizontalOffsetDir = -1;
-        }
 
         Vector3 targetPos = new Vector3(target.transform.position.x + horizontalOffset * horizontalOffsetDir, target.transform.position.y + posY, -100);
         transform.position = Vector3.Lerp(transform.position, targetPos, Time.deltaTime * smoothTime * 100);
-        rectTransform.position = (Vector2)transform.position;
     }
 }
