a69184bc43b5623f78292ccd84bf32df43259065
fxd <mygoddess007700@gmail.com>
2022-05-05 00:42:05 +0800

    :sparkles: refactor: 命令系统，用于输入接收器或人物控制器向玩家或人物传递命令

---
+++ Assets/Scripts/ActionGame/Command/Command.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Command
+{
+    // public abstract void Execute(GirlHero player);
+}
---
+++ Assets/Scripts/ActionGame/Command/TranslationCommand/RepelCommand.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepelCommand : TranslationCommand
+{
+    public RepelCommand(float _x, float _y) : base(_x, _y)
+    {
+
+    }
+}
