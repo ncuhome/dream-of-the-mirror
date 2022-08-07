5623e391ba8c21307f4e639d0ef4d1d5b3edce95
fxd <mygoddess007700@gmail.com>
2022-08-07 14:15:14 +0800

    :sparkles: feat: 简单通过PlayerPrefs添加存档功能

---
+++ Assets/Scripts/Static/StaticData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticData : MonoBehaviour
+{
+    public static int storeSceneIndex;
+    public static int storePoint;
+}
